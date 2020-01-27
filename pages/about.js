d561756768be5e517cff982b4919e2e466cecaa8
Josh Piedimonte <joshpied@gmail.com>
2020-01-27 14:47:55 -0500

    init

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Bit Prices</h1>
+      <p>Application to view Bitcoin prices</p>
+    </div>
+  </Layout>
+);
+
+export default About;
